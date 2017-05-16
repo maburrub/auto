1a579fd6a344ecf4cba9ef25207ddbc2e147f1d2
Mohammed Abualrob <mohammed@unity3d.com>
2017-05-16 15:32:48 -0700

    Galaxy initial setup

--- src/UpdateBundleID.cs
+++ src/UpdateBundleID.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿// C# example:
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.Callbacks;
 
@@ -9,7 +10,6 @@ public class UpdateBundleID {
 		string filename = pathToBuiltProject + "/Contents/Info.plist";
 		var fileContents = System.IO.File.ReadAllText(filename);
 		fileContents = fileContents.Replace("unity.Unity3d.IAPDemo", "com.unity3d.unityiap.mac.demo"); 
-		fileContents = fileContents.Replace("com.Company.ProductName", "com.unity3d.unityiap.mac.demo"); 
 		System.IO.File.WriteAllText(filename, fileContents);
 	}
 }
