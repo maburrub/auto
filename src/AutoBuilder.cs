748a9f391e1b074bbbf846e6a67aaf98e784fc74
Mohammed Abualrob <mohammed@unity3d.com>
2017-05-19 11:40:07 -0700

    More facebook stuff

--- src/AutoBuilder.cs
+++ src/AutoBuilder.cs
@@ -26,29 +26,28 @@ public static class AutoBuilder {
 
 	static void PerformWin64Build ()
 	{
-		//EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneWindows);
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
 		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/Win64/" + GetProjectName() + ".exe",BuildTarget.StandaloneWindows64,BuildOptions.None);
 	}
  
 	static void PerformMacBuild ()
 	{
-		//EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXUniversal);
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneOSXUniversal);
+	    PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Standalone, "com.unity3d.unityiap.mac.demo");
 		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/OSX-Universal/" + GetProjectName() + ".app",BuildTarget.StandaloneOSXUniversal,BuildOptions.None);
 	}
  
 	static void PerformFacebookWebglBuild ()
-	{
-		//targetPlatform (webgl or gameroom)
-		//package build for uploading
-		//appID
-		//Note we can use BuildOptions.AutoRunPlayer
-		//BuildTarget : facebook or gameroom ?
-		
-		PlayerSettings.Facebook.sdkVersion = "7.9.4";	
-		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Facebook, "281077639005121");	
-		
-		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Facebook, BuildTarget.WebGL);	
-		
+	{		
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Facebook, BuildTarget.WebGL);
+		//EditorUserBuildSettings.facebookCreatePackageForSubmission = true;
+		//EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Facebook, BuildTarget.StandaloneWindows);	
+
+		PlayerSettings.Facebook.sdkVersion = "7.9.4";
+		List<string> appIds = new List<string>();
+		appIds.Add("281077639005121");
+		Facebook.Unity.Settings.FacebookSettings.AppIds = appIds;
+				
 		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = GetScenePaths();
         buildPlayerOptions.locationPathName = "Builds/facebook";
@@ -60,31 +59,31 @@ public static class AutoBuilder {
 
 	static void PerformiOSBuild ()
 	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.iOS, BuildTarget.iOS);
 	    PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, "com.unity3d.unityiap.demo");
 	    PlayerSettings.iOS.appleDeveloperTeamID = "3XGDM87KDL";
 	    PlayerSettings.iOS.appleEnableAutomaticSigning = true;
-		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.iOS);
-		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/iOS",BuildTarget.iOS,BuildOptions.None);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/iOS",BuildTarget.iOS, BuildOptions.None);
 	}
 
 	static void PerformGooglePlayBuild ()
 	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.eight.bit.avenue.amorcam");
-		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
 		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/google_play.apk",BuildTarget.Android,BuildOptions.None);
 	}
 	
 	static void PerformFrenchLocaleBuild ()
 	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.eight.bit.avenue.amorcam");
-		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
 		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/fr_locale.apk",BuildTarget.Android,BuildOptions.None);
 	}
 	
 	static void PerformGalaxyBuild ()
 	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
 		PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.unity.purchasing.samsungappsbasic");
-		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
-		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/galaxy.apk",BuildTarget.Android,BuildOptions.None);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "Builds/galaxy.apk",BuildTarget.Android, BuildOptions.None);
 	}
 }
--- src/UpdateBundleID.cs
+++ src/UpdateBundleID.cs
@@ -10,6 +10,7 @@ public class UpdateBundleID {
 		string filename = pathToBuiltProject + "/Contents/Info.plist";
 		var fileContents = System.IO.File.ReadAllText(filename);
 		fileContents = fileContents.Replace("unity.Unity3d.IAPDemo", "com.unity3d.unityiap.mac.demo"); 
+		fileContents = fileContents.Replace("com.Company.ProductName", "com.unity3d.unityiap.mac.demo"); 
 		System.IO.File.WriteAllText(filename, fileContents);
 	}
 }
