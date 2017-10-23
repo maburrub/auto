2bd1e797ed6019966bc598a96c97ae75d672cb10
Mohammed Abualrob <mohammed@unity3d.com>
2017-10-22 20:35:58 -0700

    Adding iapstores BuildConfig.json and buildplugin.pl

---
+++ src/buildplugin.pl
@@ -0,0 +1,8 @@
+#use SDKDownloader;
+
+# Download the Tizen SDK
+#SDKDownloader::PrepareSDK("tizen-sdk", "2.4.0", "./artifacts");
+
+# Build the plugin
+#system("make build") == 0
+#    or die "system \"make build\" failed: $?";
