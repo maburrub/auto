0024be9585be46fcb5e9b0f8a109ee10dd27e2ed
maburrub <maburrub@yahoo.com>
2017-11-28 16:03:06 -0800

    creating fresh unity projects and renaming the demo app back to IAPDemo

--- src/IAPDemo.cs
+++ src/IAPDemo.cs
@@ -26,7 +26,7 @@ using UnityEngine.Purchasing.Security;
 /// To use with your account, configure the product ids (AddProduct).
 /// </summary>
 [AddComponentMenu("Unity IAP/Demo")]
-public class IAPAuto : MonoBehaviour, IStoreListener
+public class IAPDemo : MonoBehaviour, IStoreListener
 {
     // Unity IAP objects
     private IStoreController m_Controller;
