f9eaefc165dd8eb4ab5008e5412ce0845edec379
maburrub <maburrub@yahoo.com>
2017-12-07 15:52:28 -0800

    Adding Unity201720p4

---
+++ iap/Unity201720p4/Assets/Plugins/UnityPurchasing/AppleTangle.cs
@@ -0,0 +1,16 @@
+#if UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE_OSX || UNITY_TVOS
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("53bYMZ26yw/ZOmeDrK2vrq8NLK/8y8LHz8DNy47BwI7axsfdjs3L3NnZgM/e3sLLgM3Bw4HP3t7Cy83PsT91sOn+RatD8Ncqg0WYDPni+0IQWt01QHzKoWXX4Zp2DJBX1lHFZqOop4Qo5ihZo6+vq6uurSyvr67yLK+uqKeEKOYoWc3Kq6+eL1yehKgFDd886f37bwGB7x1WVU3eY0gN4p6/qK37qqS9pO/e3sLLjufAzYCfgo7Ny9zax8jHzc/ay47ewcLHzdfCy47nwM2An4ieiqit+6qlvbPv3trGwdzH2tefuJ66qK37qq29o+/enZj0nsyfpZ6nqK37qqi9rPv9n72OwciO2sbLjtrGy8COz97ewsfNz6ieoait+7O9r69Rqquera+vUZ6zhCjmKFmjr6+rq66ezJ+lnqeorfu4nrqorfuqrb2j797ewsuO/MHB2m7NndlZlKmC+EV0oY+gdBTdt+EbypuNu+W797MdOllYMjBh/hRv9v7Ayo7NwcDKx9rHwcDdjsHIjtvdy7ErLSu1N5PpmVwHNe4ggnofPrx21J4sr9ieoKit+7Ohr69RqqqtrK8uuoV+x+k62KdQWsUjgO4IWenj0YDuCFnp49Gm8J6xqK37s42qtp6469Cx4sX+OO8natrMpb4t7ymdJC/Xjs/d3dvDy92Oz83Ny97az8DNy9rHyMfNz9rLjszXjs/A147ez9zax8jHzc/ax8HAju/b2sbB3Mfa15+m8J4sr7+orfuzjqosr6aeLK+qnopMRX8Z3nGh60+JZF/D1kNJG7m5d5jRbyn7dwk3F5zsVXZ73zDQD/w7MNSiCukl9Xq4mZ1laqHjYLrHf/cJq6fSue74v7DafRkljZXpDXvBH572QvSqnCLGHSGzcMvdUcnwyxKbnJ+anp2Y9LmjnZuenJ6XnJ+antzPzdrHzcuO3drP2svDy8Da3YCeq66tLK+hrp4sr6SsLK+vrko/B6eeLKoVniytDQ6trK+srK+snqOop6qovaz7/Z+9nr+orfuqpL2k797ek4jJjiSdxFmjLGFwRQ2BV/3E9crMwsuO3drPwMrP3MqO2svcw92OzyW3J3BX5cJbqQWMnqxGtpBW/qd9Id0vzmi19aeBPBxW6uZezpYwu1upQtOXLSX9jn2Wah8RNOGkxVGFUskhphqOWWUCgo7B3hiRr54iGe1hgZ4vbaimhaivq6uprKyeLxi0Lx2YN+KD1hlDIjVyXdk1XNh82Z7hb6aFqK+rq6msr7iwxtra3t2UgYHZGbUTPeyKvIRpobMY4zLwzWblLrmInoqorfuqpb2z797ewsuO7cvc2gZy0IybZIt7d6F4xXoMio2/WQ8Cju3vniyvjJ6jqKeEKOYoWaOvr6+hM5NdheeGtGZQYBsXoHfwsnhlkxuUA1qhoK48pR+PuIDae5Kjdcy4qK37s6CquKq6hX7H6TrYp1BaxSOOz8DKjs3L3NrHyMfNz9rHwcCO3t7Cy478wcHaju3vnrC5o56Ynpqc3sLLju3L3NrHyMfNz9rHwcCO79tnt9xb86B70fE1XIutFPsh4/OjX9HvBjZXf2TIMorFv34NFUq1hG2x/gQke3RKUn6nqZke29uP");
+        private static int[] order = new int[] { 27,38,36,15,50,47,34,24,9,44,15,47,44,41,29,38,44,57,20,43,49,28,32,31,25,40,39,47,33,53,58,49,52,39,40,46,53,47,53,56,42,44,55,51,56,47,48,48,59,53,54,56,52,53,58,59,59,58,59,59,60 };
+        private static int key = 174;
+
+        public static byte[] Data() {
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
+#endif
